5115b2266f1824a50dfb3c07e2a794ccce68f8e9
Gabriel Pereira <adm1nkash1m1r@gmail.com>
2023-03-14 11:51:41 -0300

    autoload

---
+++ banco.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once 'autoload.php';
+
+use Alura\Banco\Modelo\Conta\Titular;
+use Alura\Banco\Modelo\Endereco;
+use Alura\Banco\Modelo\CPF;
+use Alura\Banco\Modelo\Conta\Conta;
+
+$endereco = new Endereco('São Luis', 'Cutim Anil', 'ec', '24');
+$gabriel = new Titular(new CPF('123.456.789-10'), 'Gabriel Pereira', $endereco);
+$primeiraConta = new Conta($gabriel);
+$primeiraConta->deposita(500);
+$primeiraConta->saca(300); // isso é ok
+
+echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
+echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
+echo $primeiraConta->recuperaSaldo() . PHP_EOL;
+
+$leo = new Titular(new CPF('698.549.548-10'), 'leonardo', $endereco);
+$segundaConta = new Conta($leo);
+var_dump($segundaConta);
+
+$outroEndereco = new Endereco('A', 'b', 'c', '1D');
+$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Abcdefg', $outroEndereco));
+unset($segundaConta);
+echo Conta::recuperaNumeroDeContas();
